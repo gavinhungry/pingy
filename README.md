ca39958b8260943dad9a604c3357e01447543f58
Gavin Lloyd <gavinhungry@gmail.com>
2014-06-14 08:41:35 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+pingy
+=====
+PNG creation and manipulation helper.
+
+License
+=======
+Released under the terms of the
+[MIT license](http://tldrlegal.com/license/mit-license). See **LICENSE**.
---
+++ pingy.js
@@ -0,0 +1,12 @@
+/**
+ * pingy: PNG creation and manipulation helper
+ * https://github.com/gavinhungry/pingy
+ *
+ * Released under the terms of the MIT license
+ */
+
+(function() {
+  'use strict';
+
+  return pingy;
+})();
